d8233e0403a88b7ff1b79e0558c40b82d0da1e03
devlulcas <lucasalvesrego.contato@gmail.com>
2021-07-15 01:16:04 -0300

    change: radical changes trying to get that working on gh pages

--- javascript/app-weather.js
+++ javascript/app-weather.js
@@ -1,5 +1,98 @@
+const ls = localStorage;
+
+//Elements
+const modal = document.querySelector("[data-modal-active]");
+const modalForm = document.getElementById("weather-configs");
+const modalOpenButton = document.getElementById("modal-open-button");
+const modalCancelButton = document.getElementById("modal-cancel-button");
+const modalSaveButton = document.getElementById("modal-save-button");
+const modalInputs = [...modalForm.elements].filter((element) => {
+  if (element.type == "text") return element;
+});
+
+//Event listeners
+window.onload = () => {
+  loadConfigUi();
+  createWeatherURL();
+};
+
+modalOpenButton.onclick = () => {
+  loadConfigUi();
+  openModal();
+};
+
+modalCancelButton.onclick = (ev) => {
+  ev.preventDefault();
+  closeModal();
+};
+
+modalSaveButton.onclick = async (ev) => {
+  ev.preventDefault();
+  saveConfigFromUiToStorage();
+  createWeatherURL();
+  await loadWeatherUi();
+  closeModal();
+};
+
+//Functions
+function openModal() {
+  modal.dataset.modalActive = "true";
+}
+
+function closeModal() {
+  modal.dataset.modalActive = "false";
+}
+
+function getConfig() {
+  const apiKey = ls.getItem("setting-apikey") || "123";
+  const country = ls.getItem("setting-country") || "brazil";
+  const state = ls.getItem("setting-state") || "bahia";
+  const city = ls.getItem("setting-city") || "salvador";
+  return {
+    apiKey,
+    country,
+    state,
+    city,
+  };
+}
+
+function setConfig(
+  apiKey = "123",
+  country = "brazil",
+  state = "bahia",
+  city = "salvador"
+) {
+  ls.setItem("setting-apikey", apiKey);
+  ls.setItem("setting-country", country);
+  ls.setItem("setting-state", state);
+  ls.setItem("setting-city", city);
+}
+
+function loadConfigUi() {
+  const storedConfig = getConfig();
+  modalInputs[0].value = storedConfig.apiKey;
+  modalInputs[1].value = storedConfig.country;
+  modalInputs[2].value = storedConfig.state;
+  modalInputs[3].value = storedConfig.city;
+}
+
+function saveConfigFromUiToStorage() {
+  const newConfig = modalInputs.map((input) => input.value.toLowerCase());
+  setConfig(...newConfig);
+}
+
+function createWeatherURL() {
+  const config = getConfig();
+  const baseUrl = `https://api.weatherbit.io/v2.0/current?`;
+  const configUrl = `city=${config.city}&country=${config.country}&state=${config.state}&lang=pt&key=${config.apiKey}`;
+  const finalurl = `${baseUrl}${configUrl}`;
+  ls.setItem("setting-finalurl", finalurl);
+}
+
+//-----------
+
 function getWeatherUrl() {
-  const url = localStorage.getItem("setting-finalurl");
+  const url = ls.getItem("setting-finalurl");
   return url;
 }
 
@@ -28,7 +121,7 @@ async function getWeatherData(url) {
 
 function changeIconTitle() {
   const icon = document.getElementById("weather-app-icon");
-  const city = localStorage.getItem("setting-city");
+  const city = ls.getItem("setting-city");
   icon.title = `Clima em ${city.charAt(0).toUpperCase()}${city.slice(1)}`;
 }
 
@@ -50,4 +143,4 @@ async function loadWeatherUi() {
 
 await loadWeatherUi();
 
-export { loadWeatherUi };
+//export { loadWeatherUi };
